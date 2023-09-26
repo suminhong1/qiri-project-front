3f30a0aa3256ba9e5dd92fb8703730ead7db3801
mayerjogn <130232731+mayerjogn@users.noreply.github.com>
2023-09-26 19:34:27 +0900

    커밋 넣을게

--- qoqiri/src/router.js
+++ qoqiri/src/router.js
@@ -1,20 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./components/Layout";
-import NotFound from "./pages/NotFound";
+import { createBrowserRouter } from 'react-router-dom';
+import Post from './pages/Post';
+import Layout from './components/Layout';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                index: true,
+                element: <BestPost />,
+            },
+            {
+                path: 'ViewPost',
+                element: <ViewPost />,
+            },
+        ],
+    },
 ]);
 
 export default router;
