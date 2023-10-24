76f516e9ef6e29bf37e5bd165f0654ec72020f46
HHOWI <minsu705@gmail.com>
2023-10-24 20:16:40 +0900

    feat:채팅구현

--- qoqiri/src/pages/ChatRoom.js
+++ qoqiri/src/pages/ChatRoom.js
@@ -1,118 +1,131 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
 import { useParams } from "react-router-dom";
 import { getChatRoom } from "../api/chat";
-import SockJS from "sockjs-client";
-import StompJs from 'stompjs';
 
 const ChatRoom = () => {
+  const [room, setRoom] = useState({});
+  const [sender, setSender] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const { roomId } = useParams();
+  const stompClient = useRef(null);
 
-    const sock = new SockJS('http://localhost:8080/ws');
-    const ws= StompJs.over(sock);
+  const chatRoomAPI = async () => {
+    const result = await getChatRoom(roomId);
+    setRoom(result.data);
+  };
 
-    const [roomSEQ, setRoomSEQ] = useState('');
-    const [room, setRoom] = useState({});
-    const [sender, setSender] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [reconnect, setReconnect] = useState(0);
+  useEffect(() => {
+    chatRoomAPI();
+  }, []);
 
-    const {id} = useParams();
+  useEffect(() => {
+    chatRoomAPI();
+  }, [roomId]);
 
-    const getChatRoomAPI = async () => {
-        const result = await getChatRoom(id);
-        setRoom(result.data);
-    }
+  useEffect(() => {
+    const socket = new SockJS("/ws");
+    stompClient.current = new Client({
+      webSocketFactory: () => socket,
+      connectHeaders: {},
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+    });
 
+    stompClient.current.onConnect = (frame) => {
+      console.log("Connected: " + frame);
+      stompClient.current.subscribe(`/sub/chat/room/${roomId}`, (message) => {
+        const recv = JSON.parse(message.body);
+        recvMessage(recv);
+      });
 
-    const sendMessage = () => {
-        ws.send('pub/chat/message', {}, JSON.stringify({
-            type: 'TALK',
-            roomSEQ: roomSEQ,
-            sender: sender,
-            message: message
-        }));
-        setMessage('');
+      stompClient.current.publish({
+        destination: "/pub/chat/message",
+        body: JSON.stringify({
+          type: "ENTER",
+          chatRoomSEQ: roomId,
+          sender: sender,
+          message: "",
+        }),
+      });
     };
 
-    const recvMessage = (recv) => {
-        setMessages([{
-            type: recv.type, sender: recv.type == 'ENTER' ? '[알림]' : recv.sender, message: recv.message }, ...messages]);
+    stompClient.current.activate();
+
+    setSender(localStorage.getItem("wschat.sender"));
+
+    return () => {
+      if (stompClient.current.active) {
+        stompClient.current.deactivate();
+      }
     };
+  }, [roomId]);
 
-    const connectWebSocket = () => {
-        ws.connect({}, (Frame) => {
-            ws.subscribe(`/sub/chat/room/${roomSEQ}`, (message) => {
-                const recv = JSON.parse(message.body);
-                recvMessage(recv);
-            });
-            ws.send('/pub/chat/message', {}, JSON.stringify({
-                type: 'ENTER',
-                roomSEQ: roomSEQ,
-                sender: sender
-            }));
-        }, (error) => {
-            if(reconnect <= 5) {
-                setTimeout(() => {
-                    console.log('connection reconnect');
-                    sock = new SockJS('/ws-stomp');
-                    ws = StompJs.over(sock);
-                    connectWebSocket();
-                    setReconnect(reconnect + 1);
-                }, 10 * 1000);
-            }
-        });
-     };
-    
-    useEffect(() => {
-         setRoomSEQ(localStorage.getItem('wschat.roomId'));
-        setSender(localStorage.getItem('wschat.sender'));
-        getChatRoomAPI();
-        connectWebSocket();
+  const sendMessage = () => {
+    stompClient.current.publish({
+      destination: "/pub/chat/message",
+      body: JSON.stringify({
+        type: "TALK",
+        chatRoomSEQ: roomId,
+        sender: sender,
+        message: message,
+      }),
+    });
+    setMessage("");
+  };
 
-        return () => {
-          if (sock) {
-            sock.close();
-          }
-        };
-      }, []);
+  const recvMessage = (recv) => {
+    setMessages((prevMessages) => [
+      {
+        type: recv.type,
+        sender: recv.type === "ENTER" ? "[알림]" : recv.sender,
+        message: recv.message,
+      },
+      ...prevMessages,
+    ]);
+  };
 
-    return (
-        <div className="container" id="app">
-          <div>
-            <h2>{room?.chatRoomSEQ}</h2>
-          </div>
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <label className="input-group-text">내용</label>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') sendMessage();
-              }}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-primary"
-                type="button"
-                onClick={sendMessage}
-              >
-                보내기
-              </button>
-            </div>
-          </div>
-          <ul className="list-group">
-            {messages.map((message, index) => (
-              <li className="list-group-item" key={index}>
-                {message.sender} - {message.message}
-              </li>
-            ))}
-          </ul>
-          <div></div>
+  return (
+    <div className="container" id="app">
+      <div>
+        <h2>{room?.name}</h2>
+      </div>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <label className="input-group-text">내용</label>
         </div>
-      );
-}
+        <input
+          type="text"
+          className="form-control"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") sendMessage();
+          }}
+        />
+        <div className="input-group-append">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={sendMessage}
+          >
+            보내기
+          </button>
+        </div>
+      </div>
+      <ul className="list-group">
+        {messages.map((msg, index) => (
+          <li className="list-group-item" key={index}>
+            {msg.sender} - {msg.message}
+          </li>
+        ))}
+      </ul>
+      <div></div>
+    </div>
+  );
+};
+
 export default ChatRoom;
--- qoqiri/src/router.js
+++ qoqiri/src/router.js
@@ -85,7 +85,7 @@ const router = createBrowserRouter([
       {
         path: "/SignupInfo",
         element: <SignupInfo />,
-      }
+      },
     ],
   },
   {
