32864ce6be52e46158248b79a60c851c77908f75
HHOWI <minsu705@gmail.com>
2023-10-31 04:01:03 +0900

    feat:채팅 구현중

--- qoqiri/src/components/test.js
+++ qoqiri/src/components/test.js
@@ -1,7 +1,5 @@
 import { useParams } from "react-router-dom";
 import { createChatRoom, requestMatching } from "../api/chat";
-import SockJS from "sockjs-client";
-import { Client } from "@stomp/stompjs";
 
 //매칭신청버튼 누를때 동작할 함수
 const test = () => {
@@ -14,20 +12,7 @@ const test = () => {
     const chatDTO = {
       postSEQ: postSEQ,
     };
-    createChatRoom(chatDTO);
-    //채팅방생성 및 신청자가 접속하는 기능
-    const socket = new SockJS("http://localhost:8080/ws/chat");
-    const stompClient = new Client({
-      webSocketFactory: () => socket,
-      connectHeaders: { token },
-      reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
-    });
-    stompClient.connect({}, () => {});
   };
-
-  // 채팅발송기능
 };
 
 //게시글
